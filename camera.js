1a8d1a3d206062840406ae2bf6e6e99d97b4d5db
hraposo36 <helder.l.raposo@gmail.com>
2019-12-07 10:42:23 +0000

    first upload

---
+++ camera.js
@@ -0,0 +1,37 @@
+// Set constraints for the video stream
+/* var constraints = { audio: true, video: { facingMode: { exact: "environment" } } } // rear camera */
+var constraints = { video: { facingMode: "user" }, audio: false }; // front camera
+ 
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+              
+// Access the device camera and stream to cameraView
+function cameraStart() {
+              
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then(function(mediaStream) {
+			var video = document.querySelector('video');
+			video.srcObject = mediaStream;
+			video.onloadedmetadata = function(e) {
+				video.play();
+			};
+		})
+	  	.catch(function(err) { 
+	  		console.log(err.name + ": " + err.message); 
+	  	}); // always check for errors at the end.
+}
+ 
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/jpeg");
+    cameraOutput.classList.add("taken");
+};
+ 
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
